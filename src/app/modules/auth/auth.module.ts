17e0e3a1807752a2dfe8ad333a5ac983d9514b5e
njmarko <marko.njegomir@uns.ac.rs>
2023-06-25 18:15:28 +0200

    feat(auth): Added login form. Set up path in environment for backend

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthRoutes } from './auth.routes';
-
-
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +12,7 @@ import { AuthRoutes } from './auth.routes';
   imports: [
     CommonModule,
     RouterModule.forChild(AuthRoutes),
+    MaterialModule
   ]
 })
 export class AuthModule { }
--- src/app/modules/auth/pages/login/login.component.spec.ts
+++ src/app/modules/auth/pages/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth-service/auth.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CurrentUserService } from '../../services/current-user-service/current-user.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +13,13 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        RouterTestingModule,
+      ],
+      providers: [AuthService, CurrentUserService],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
