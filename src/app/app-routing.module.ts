e6d56d79fc670504d95e962043d9cab8e11f446f
njmarko <marko.njegomir@uns.ac.rs>
2023-06-25 17:13:30 +0200

    feat(auth): Added auth componnent and material design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'auth',
+        // component: LoginComponent,
+        loadChildren: () =>
+            import("./modules/auth/auth.module").then((m) => m.AuthModule)
+        
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
